94e3cd7a0575d58aa61d815fd295230109f57624
iriso1 <114158685+iriso1@users.noreply.github.com>
2024-08-12 18:37:19 +0200

    fix db config

--- backend-missbug/config/index.js
+++ backend-missbug/config/index.js
@@ -4,14 +4,14 @@ import configDev from './dev.js'
 
 export let config
 
-if (process.env.NODE_ENV.includes('production')) {
+if (process.env.NODE_ENV?.includes('production')) {
   console.log('mongoDB mode production:', process.env.NODE_ENV)
   config = configProd
 } else {
   console.log('mongoDB mode not production:', process.env.NODE_ENV)
   config = configDev
 }
-console.log('mongoDB url:', process.env.MONGO_URL)
+// console.log('mongoDB url:', process.env.MONGO_URL)
 // config.isGuestMode = true
 
 
